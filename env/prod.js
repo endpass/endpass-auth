92ad42688ec31cc7e06a073a25d61faea7e5cd58
Alexander Kazimir <alex_kazik@mail.ru>
2019-02-25 06:59:28 +0300

    fix: remove local mode for production

--- env/prod.js
+++ env/prod.js
@@ -11,14 +11,18 @@ const hdKeyMnemonic = {
   path: `m/44'/60'/0'/0`, // Derivation path
 };
 
-const googleClientId = '885568627115-sin32pl0317peotp05r69gbmsb00atcj.apps.googleusercontent.com';
+const googleClientId =
+  '885568627115-sin32pl0317peotp05r69gbmsb00atcj.apps.googleusercontent.com';
 
 const gitClientId = '346108128ad51a5eccd1';
+
+const isProduction = true;
 
 module.exports = {
   infura,
   identity,
   hdKeyMnemonic,
   googleClientId,
-  gitClientId
+  gitClientId,
+  isProduction,
 };
