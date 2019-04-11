55fc67dc21e63bd06f12e2449c7e6aef1fa320fd
lamartire <lamartire@gmail.com>
2019-04-11 16:55:27 +0300

    feat: add widget component and markup for it (without icons)

---
+++ src/components/widget/Accordion.vue
@@ -0,0 +1,36 @@
+<template>
+  <section class="accordion">
+    <slot name="control" />
+    <section
+      v-show-slide="!collapsed"
+      class="accordion-content"
+      :style="{ maxHeight: maxHeight }"
+    >
+      <slot />
+    </section>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Accordion',
+
+  props: {
+    maxHeight: {
+      type: String,
+      default: 'none',
+    },
+
+    collapsed: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style lane="postcss">
+.accordion-content {
+  overflow-y: auto !important;
+}
+</style>
