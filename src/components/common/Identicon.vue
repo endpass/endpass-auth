df5138056a67e2ea83add257bae7f57d2c98b2e5
lamartire <lamartire@gmail.com>
2019-04-16 14:45:14 +0300

    feat(widget): add avatars to addresses in widget

---
+++ src/components/common/Identicon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="identicon">
+    <img :src="image" :alt="address" :title="address" />
+  </div>
+</template>
+
+<script>
+import blockie from 'ethereum-blockies-base64';
+
+export default {
+  name: 'Identicon',
+
+  props: {
+    address: {
+      type: String,
+      default: '',
+    },
+  },
+
+  computed: {
+    image() {
+      return this.address && blockie(this.address);
+    },
+  },
+};
+</script>
+
+<style lang="postcss">
+.identicon {
+  display: flex;
+  align-items: center;
+  width: 18px;
+  height: 18px;
+  border-radius: 50%;
+  overflow: hidden;
+  border: 1px solid #e2e2e2;
+  box-sizing: content-box;
+}
+</style>
