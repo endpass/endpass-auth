690a36190414c29682c82ffa5a0a31c83e5270df
lamartire <lamartire@gmail.com>
2019-06-07 09:51:28 +0300

    feat: add promise-based sw registration

--- src/util/e2eWorker.js
+++ src/util/e2eWorker.js
@@ -1,14 +1,38 @@
-function registerE2EWorker() {
-  if (navigator.serviceWorker) {
+// TODO: move it to the utils
+export function registerE2EWorker() {
+  return new Promise(async resolve => {
+    if (!navigator.serviceWorker) return resolve();
+
     const workerUrl = `${ENV.VUE_APP_E2E_CONNECT_BASE_URL}/e2e-sw.js`;
 
     try {
-      navigator.serviceWorker.register(workerUrl);
+      const registration = await navigator.serviceWorker.register(workerUrl);
+      const activateHandler = () =>
+        setTimeout(() => {
+          const isCurrentScope = new RegExp(window.location.origin).test(
+            registration.scope,
+          );
+          const isActivated = Boolean(registration.active);
+
+          if (isCurrentScope && isActivated) {
+            return resolve();
+          }
+
+          return activateHandler();
+        }, 500);
+
+      activateHandler();
     } catch (err) {
-      /* eslint-disable-next-line */
+      /* eslint-disable */
       console.error(`Can't register e2e worker from ${workerUrl}!`);
+      console.error(err);
+      /* eslint-enable */
+
+      return resolve();
     }
-  }
+  });
 }
 
-export default registerE2EWorker;
+export default {
+  registerE2EWorker,
+};
