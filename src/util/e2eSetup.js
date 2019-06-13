c4c636bb027535a5c04a4e7d1b0ff8227ee3877a
lamartire <lamartire@gmail.com>
2019-06-13 17:02:15 +0300

    fix: update e2e utils and fix e2e setup entry

--- src/util/e2eSetup.js
+++ src/util/e2eSetup.js
@@ -1,7 +1,40 @@
 import { SWController, SWControllerDuplexBridge } from '@endpass/e2e-utils';
 
+const registerWorker = url => {
+  return new Promise(async resolve => {
+    if (!window.navigator.serviceWorker) return resolve(null);
+
+    try {
+      const registration = await window.navigator.serviceWorker.register(url);
+      const updateHandler = () => {
+        registration.removeEventListener('updatefound', updateHandler);
+
+        return resolve(registration.active);
+      };
+
+      if (registration.active) {
+        return resolve(registration.active);
+      }
+
+      registration.addEventListener('updatefound', updateHandler);
+    } catch (err) {
+      /* eslint-disable */
+      console.error(`Service worker ${url} was not registered!`);
+      console.error(err.message);
+      console.error(err);
+      /* eslint-enable */
+      return resolve(null);
+    }
+  });
+};
+
 const e2eSetup = async () => {
-  const sw = new Worker(`${ENV.VUE_APP_E2E_CONNECT_BASE_URL}/sw-e2e.js`);
+  const sw = await registerWorker(
+    `${ENV.VUE_APP_E2E_CONNECT_BASE_URL}/sw-e2e.js`,
+  );
+
+  if (!sw) return;
+
   const controller = new SWController(sw);
 
   const swDuplexBridge = new SWControllerDuplexBridge({
