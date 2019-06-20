54e637f825106ec98c0a9fb407dd5095d9e10164
Mona Lisa <Lenseg1@gmail.com>
2019-06-20 14:50:47 +0300

    chore: removed seed timer

--- src/components/forms/CreateWallet.vue
+++ src/components/forms/CreateWallet.vue
@@ -53,23 +53,19 @@
         </p>
       </div>
       <v-button @click="onContinue">
-        Continue {{ timeoutTitle }}
+        Continue
       </v-button>
     </div>
   </div>
 </template>
 
 <script>
-import VueTimers from 'vue-timers/mixin';
 import { mapActions } from 'vuex';
 import VButton from '@/components/common/VButton.vue';
 import Message from '@/components/common/Message.vue';
 import FormControls from '@/components/common/FormControls.vue';
 import VInput from '@/components/common/VInput.vue';
 import FormField from '@/components/common/FormField.vue';
-
-const SEED_PHRASE_TIMEOUT_SEC = 10;
-const UPDATE_SEED_PHRASE_INTERVAL_MSEC = 1000;
 
 export default {
   name: 'CreateWalletForm',
@@ -80,10 +76,7 @@ export default {
     passwordConfirm: '',
     password: '',
     seedKey: null,
-    isShowSeed: false,
     isLoading: false,
-    isTimerActive: false,
-    timerValue: SEED_PHRASE_TIMEOUT_SEC,
   }),
 
   computed: {
@@ -96,9 +89,6 @@ export default {
     primaryButtonLabel() {
       return this.isLoading ? 'Loading...' : 'Create Wallet';
     },
-    timeoutTitle() {
-      return this.isTimerActive ? `(${this.timerValue})` : '';
-    },
   },
 
   methods: {
@@ -108,11 +98,7 @@ export default {
         this.isLoading = true;
         try {
           this.error = '';
-          this.timerValue = SEED_PHRASE_TIMEOUT_SEC;
           this.seedKey = await this.createWallet({ password: this.password });
-          this.isTimerActive = true;
-          this.isShowSeed = true;
-          this.$timer.start('seedPhrase');
         } catch (e) {
           console.error(e);
           this.error = 'Something broken, when trying to create new Wallet';
@@ -123,28 +109,9 @@ export default {
     onContinue() {
       this.setWalletCreated();
     },
-    handleSeedPhraseTimer() {
-      this.timerValue = this.timerValue - 1;
-
-      if (this.timerValue <= 0) {
-        this.isTimerActive = false;
-        this.$timer.stop('seedPhrase');
-        this.seedKey = null;
-      }
-    },
   },
 
-  mixins: [VueTimers],
-
-  timers: {
-    seedPhrase: {
-      repeat: true,
-      time: UPDATE_SEED_PHRASE_INTERVAL_MSEC,
-      callback() {
-        this.handleSeedPhraseTimer();
-      },
-    },
-  },
+  mixins: [],
 
   components: {
     FormField,
