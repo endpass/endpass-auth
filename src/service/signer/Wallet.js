765966a931e881f3f7ac6ce9adc35c4e5100deac
lamartire <lamartire@gmail.com>
2019-07-03 17:14:01 +0300

    feat(sign): draw request messages and transaction form

--- src/service/signer/Wallet.js
+++ src/service/signer/Wallet.js
@@ -4,8 +4,7 @@ import Tx from 'ethereumjs-tx';
 import keystoreKeyGen from '@endpass/utils/keystoreKeyGen';
 import isV3 from '@endpass/utils/isV3';
 import i18n from '@/locales/i18n';
-
-import web3 from '@/service/signer/web3';
+import web3 from '@/class/singleton/web3';
 
 const { isAddress, bytesToHex, numberToHex } = Web3.utils;
 
