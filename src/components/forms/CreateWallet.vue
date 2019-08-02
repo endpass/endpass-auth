ddc0c00ced689ae458fe9c9ba6e9e41ef7f5fdc4
lamartire <lamartire@gmail.com>
2019-08-02 18:10:36 +0300

    feat: update seed phrase form

--- src/components/forms/CreateWallet.vue
+++ src/components/forms/CreateWallet.vue
@@ -61,22 +61,47 @@
       />
       <form-item>
         <div class="box">
-          <span
-            class="create-wallet-subtitle"
+          <p
+            class="create-wallet-subtitle v-body is-small"
             v-html="$t('components.createWallet.recoverySubtitle')"
           />
-          <br>
-          <p
-            class="create-wallet-code"
+          <ul
+            class="create-wallet-seed"
             data-test="seed-phrase"
           >
-            {{ seedKey }}
-          </p>
+            <li
+              v-for="word in splittedSeedKey"
+              :key="word"
+            >
+              <v-tag skin="light-gray">
+                {{ word }}
+              </v-tag>
+            </li>
+          </ul>
+          <div
+            v-if="seedTemplateUrl"
+            class="create-wallet-template-download"
+          >
+            <v-icon-control
+              icon="pdf"
+              :href="seedTemplateUrl"
+              target="_blank"
+            >
+              {{ $t('components.createWallet.downloadTemplate') }}
+            </v-icon-control>
+          </div>
+        </div>
+        <div class="create-wallet-seed-check">
+          <v-checkbox v-model="isSeedConfirmed">
+            {{ $t('components.createWallet.seedConfirmation') }}
+          </v-checkbox>
         </div>
       </form-item>
       <v-button
+        :disabled="!isSeedConfirmed"
         type="button"
         size="big"
+        data-test="continue-button"
         @click="onContinue"
       >
         {{ $t('global.continue') }}
@@ -92,17 +117,22 @@ import Message from '@/components/common/Message';
 import FormItem from '@/components/common/FormItem';
 import VSpacer from '@/components/common/VSpacer';
 import VInput from '@endpass/ui/kit/VInput';
+import VTag from '@endpass/ui/kit/VTag';
+import VIconControl from '@endpass/ui/kit/VIconControl';
+import VCheckbox from '@endpass/ui/kit/VCheckbox';
 import formMixin from '@/mixins/form';
 
 export default {
   name: 'CreateWalletForm',
 
   data: () => ({
+    seedTemplateUrl: null,
     email: '',
     error: '',
     passwordConfirm: '',
     password: '',
     seedKey: null,
+    isSeedConfirmed: false,
     isShowSeed: false,
     isLoading: false,
   }),
@@ -111,18 +141,30 @@ export default {
     canSubmit() {
       return this.isPasswordEqual && !this.isLoading && this.isFormValid;
     },
+
     isPasswordEqual() {
       return this.password && this.password === this.passwordConfirm;
     },
+
     primaryButtonLabel() {
       return this.isLoading
         ? this.$i18n.t('global.loading')
         : this.$i18n.t('components.createWallet.createWallet');
     },
+
+    splittedSeedKey() {
+      if (!this.seedKey) return [];
+
+      return this.seedKey.split(/\s/);
+    },
   },
 
   methods: {
-    ...mapActions(['createInitialWallet', 'setWalletCreated']),
+    ...mapActions([
+      'createInitialWallet',
+      'setWalletCreated',
+      'getSeedTemplateUrl',
+    ]),
 
     async onCreateWallet() {
       if (!this.canSubmit) {
@@ -144,35 +186,64 @@ export default {
       this.isLoading = false;
     },
     onContinue() {
+      if (!this.isSeedConfirmed) return;
+
       this.setWalletCreated();
     },
+  },
+
+  async mounted() {
+    this.seedTemplateUrl = await this.getSeedTemplateUrl();
   },
 
   mixins: [formMixin],
 
   components: {
     FormItem,
+    Message,
     VInput,
     VSpacer,
     VButton,
-    Message,
+    VTag,
+    VIconControl,
+    VCheckbox,
   },
 };
 </script>
 <style lang="postcss">
 .create-wallet-subtitle {
-  /* TODO: change to grey-6 after update ui-kit*/
-  color: #697077;
-  line-height: 20px;
+  color: var(--endpass-ui-color-grey-6);
   text-align: center;
-  display: inline-block;
+  margin-bottom: 28px;
+}
+
+.create-wallet-seed {
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+  text-align: center;
+  list-style: none;
+  margin: 0 -4px 16px;
+}
+
+.create-wallet-seed li {
+  margin: 4px;
+}
+
+.create-wallet-template-download {
+  text-align: center;
   margin-bottom: 24px;
 }
 
-.create-wallet-code {
-  font-weight: bold;
-  margin-bottom: 32px;
-  text-align: center;
-  line-height: 20px;
+.create-wallet-template-download a {
+  display: inline-flex !important;
+}
+
+.create-wallet-template-download svg {
+  color: #e5e9ef;
+}
+
+.create-wallet-seed-check {
+  margin-bottom: 24px;
 }
 </style>
