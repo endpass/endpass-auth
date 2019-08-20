8e525d685558e878b65685f998e4805ef5364dc3
lamartire <lamartire@gmail.com>
2019-08-20 17:31:50 +0300

    feat: setup environment for trx e2e tests

--- src/service/web3.js
+++ src/service/web3.js
@@ -1,11 +1,15 @@
 import Web3 from 'web3';
-import Network from '@endpass/class/Network';
+import { Network, ProviderFactory } from '@endpass/class';
 
 export const web3 = new Web3();
 
 export const setWeb3Network = (net = Network.NET_ID.MAIN) => {
+  if (web3.currentProvider && web3.currentProvider.isMock) {
+    return;
+  }
+
   const netUrl = Network.NETWORK_URL_HTTP[net][0];
-  const provider = new web3.providers.HttpProvider(netUrl);
+  const provider = ProviderFactory.getInstance(netUrl);
 
   web3.setProvider(provider);
 };
