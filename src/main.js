6ce0ffe7fdba541dfb5a6fea9eff6cb470cf2af9
lamartire <lamartire@gmail.com>
2019-08-20 17:31:50 +0300

    feat: setup environment for trx e2e tests

--- src/main.js
+++ src/main.js
@@ -9,12 +9,28 @@ import i18n from '@/locales/i18n';
 
 (async () => {
   try {
-    if (window.parent.e2eBridge) {
-      console.warn('AUTH is working on E2E MODE');
-      await window.parent.e2eBridge.awaitClientResume();
-      window.XMLHttpRequest = window.parent.XMLHttpRequest;
-      window.fetch = window.parent.fetch;
+    if (!window.parent.e2eBridge) {
+      throw new Error();
     }
+
+    /* eslint-disable-next-line */
+    const { web3, setWeb3Network } = require('@/service/web3');
+
+    /* eslint-disable-next-line */
+    console.warn('AUTH is working on E2E MODE');
+
+    window.Cypress = true;
+    window.parent.setWeb3AuthProvider = net => {
+      setWeb3Network(net);
+      window.parent.web3AuthNet = net;
+      window.parent.web3Auth = web3;
+    };
+
+    await window.parent.e2eBridge.awaitClientResume();
+
+    window.XMLHttpRequest = window.parent.XMLHttpRequest;
+    window.fetch = window.parent.fetch;
+    /* eslint-disable-next-line */
   } catch (e) {}
 
   Vue.use(validation);
