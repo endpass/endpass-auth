fc6e615d02a2a5f39730fcb1c60334f88b350f6e
idudiq <idudiq@gmail.com>
2019-09-20 19:06:17 +0300

    fix: declare request config, remove not used langs

--- src/class/singleton/request/request.js
+++ src/class/singleton/request/request.js
@@ -1,13 +1,14 @@
+// @ts-check
 import http from './http';
 import { ORIGIN_HOST } from '@/constants';
 import Request from '@/class/Request';
 import defaultConfig from '@/class/singleton/request/defaultConfig';
 
-const configWithHeaders = {
+const config = {
   ...defaultConfig,
   headers: {
     'x-connect-lib-host': ORIGIN_HOST,
   },
 };
 
-export default new Request({ http, config: configWithHeaders });
+export default new Request({ http, config });
--- src/class/singleton/request/requestSkipPermission.js
+++ src/class/singleton/request/requestSkipPermission.js
@@ -1,3 +1,4 @@
+// @ts-check
 import http from './http';
 import { parseUrl } from '@/util/dom';
 import Request from '@/class/Request';
@@ -5,11 +6,11 @@ import defaultConfig from '@/class/singleton/request/defaultConfig';
 
 const { origin } = parseUrl(ENV.VUE_APP_WALLET_URL);
 
-const configSkipPermission = {
+const config = {
   ...defaultConfig,
   headers: {
     'x-connect-lib-host': origin,
   },
 };
 
-export default new Request({ config: configSkipPermission, http });
+export default new Request({ config, http });
