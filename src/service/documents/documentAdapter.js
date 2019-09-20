ca73d01684db7253dc5ea5bdb3de7ffbd1540fa7
idudiq <idudiq@gmail.com>
2019-09-20 17:05:48 +0300

    test: add documents specs

--- src/service/documents/documentAdapter.js
+++
@@ -1,7 +0,0 @@
-export default function documentAdapter(item) {
-  return {
-    ...item,
-    imgPath: `${ENV.VUE_APP_IDENTITY_API_URL}/documents/${item.id}/front/file`,
-    imgPathBack: `${ENV.VUE_APP_IDENTITY_API_URL}/documents/${item.id}/back/file`,
-  };
-}
---
+++ tests/unit/fixtures/documents.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export const docStatusesMap = {
+  docId: {
+    front: { status: 'Uploaded' },
+    back: { status: 'Uploaded' },
+  },
+};
