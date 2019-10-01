9d69645564c8486d49c0416c4a8e0f855e18554e
idudiq <idudiq@gmail.com>
2019-10-01 14:06:28 +0300

    fix: switch from opt email code forms

--- src/components/forms/CompositeAuth/Code/Code.vue
+++ src/components/forms/CompositeAuth/Code/Code.vue
@@ -32,7 +32,7 @@ export default {
 
   computed: {
     currentForm() {
-      return OtpCode; // this.otpEmail ? OtpCode : EmailCode;
+      return this.otpEmail ? OtpCode : EmailCode;
     },
 
     otpEmail() {
