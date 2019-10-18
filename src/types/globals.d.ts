51c20bd225a667076073494b6c7e1dc9764372c8
Danil Kosterin <raymondcostner@gmail.com>
2019-10-18 16:45:24 +0300

    refactor: remove utils types definition from globals.d.ts

--- src/types/globals.d.ts
+++ src/types/globals.d.ts
@@ -12,29 +12,6 @@ declare namespace ENV {
   const VUE_APP_SHOW_VERSION_INFO: boolean;
 }
 
-declare module '@endpass/utils/date' {
-  function formateDate(date: Date): string;
-}
-
-declare module '@endpass/utils/mapToQueryString' {
-  function mapToQueryString(url: string, params: object): string;
-  export = mapToQueryString;
-}
-
-declare module '@endpass/utils/generators' {
-  function repeatWithInterval(ms: number): Generator;
-  function sleep<T>(ms: number, result: T): Promise<T>;
-}
-
-declare module '@endpass/utils/walletGen' {
-  function createComplex(password: string, options: object): {
-    v3KeystoreHdWallet: v3Keystore,
-    v3KeystoreChildWallet: v3Keystore,
-    encryptedSeed: string,
-    seedKey: string,
-  };
-}
-
 interface Window {
   e2eBridge: object;
 }
