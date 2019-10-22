555d52a4e88cde374a4bbacd12569c6dbb2ee85d
idudiq <idudiq@gmail.com>
2019-10-22 18:49:29 +0300

    refactor: add closable for iframe oauth

--- src/components/screens/Loader.vue
+++ src/components/screens/Loader.vue
@@ -7,22 +7,13 @@
 <script>
 import pkg from '@/../package.json';
 import VFrame from '@/components/common/VFrame';
-import { coreStore } from '@/store';
 
 export default {
   name: 'Loader',
 
-  coreStore,
-
   computed: {
     version() {
       return pkg.version;
-    },
-  },
-
-  methods: {
-    handleClose() {
-      this.$options.coreStore.dialogClose();
     },
   },
 
