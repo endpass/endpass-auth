a45b1b86ed50b68d166d752312c8f185d612050b
dudiq <idudiq@gmail.com>
2019-11-22 16:03:36 +0300

    feat: add document upload params (#240)

--- src/constants/translates.js
+++ src/constants/translates.js
@@ -7,6 +7,8 @@ const { $t, createGetters } = TranslateObjectFactory;
 export const CONSTANT_TRANSLATES = createGetters({
   [DOC_TYPES.PASSPORT]: $t('constants.documentType.passport'),
   [DOC_TYPES.DRIVER_LICENSE]: $t('constants.documentType.driverLicense'),
+  [DOC_TYPES.PROOF_OF_ADDRESS]: $t('constants.documentType.proofOfAddress'),
+  [DOC_TYPES.ID_CARD]: $t('constants.documentType.idCard'),
 });
 
 export default {
