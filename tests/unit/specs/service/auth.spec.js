bfb9cf802e4596fe2d34dc2d0b1b71e893388275
Danil Kosterin <raymondcostner@gmail.com>
2020-01-13 15:18:43 +0300

    test(unit): add checks for fail case of auth test

--- tests/unit/specs/service/auth.spec.js
+++ tests/unit/specs/service/auth.spec.js
@@ -155,10 +155,10 @@ describe('auth service', () => {
 
   describe('authWithGoogle', () => {
     const url = `${identityBaseUrl}/auth/google`;
+    const token = '123';
 
     it('should return 200 OK', async () => {
       expect.assertions(1);
-      const token = '123';
 
       axiosMock.onPost(url).reply(200, {
         success: true,
@@ -169,14 +169,29 @@ describe('auth service', () => {
         success: true,
       });
     });
+
+    it('should throw error on invalid request', async () => {
+      expect.assertions(1);
+      const message = 'Incorrect request';
+
+      axiosMock.onPost(url).reply(500, {
+        message,
+      });
+
+      try {
+        await authService.authWithGoogle(token);
+      } catch (e) {
+        expect(e.message).toBe(message);
+      }
+    });
   });
 
   describe('authWithGithub', () => {
     const url = `${identityBaseUrl}/auth/github`;
+    const code = '123';
 
     it('should return 200 OK', async () => {
       expect.assertions(1);
-      const code = '123';
 
       axiosMock.onPost(url).reply(200, {
         success: true,
@@ -186,6 +201,21 @@ describe('auth service', () => {
       expect(res).toEqual({
         success: true,
       });
+    });
+
+    it('should throw error on invalid request', async () => {
+      expect.assertions(1);
+      const message = 'Incorrect request';
+
+      axiosMock.onPost(url).reply(500, {
+        message,
+      });
+
+      try {
+        await authService.authWithGitHub(code);
+      } catch (e) {
+        expect(e.message).toBe(message);
+      }
     });
   });
 });
