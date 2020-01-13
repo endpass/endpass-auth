7072dea436f3a50a4607ecc2ce41f0d5a36ee639
idudiq <idudiq@gmail.com>
2020-01-13 17:12:39 +0300

    fix: return headers for each request correctly

--- src/class/singleton/request/request.js
+++ src/class/singleton/request/request.js
@@ -4,13 +4,22 @@ import http from './http';
 import host from '@/class/singleton/host';
 import defaultConfig from '@/class/singleton/request/defaultConfig';
 
-const config = {
+const requestConfig = {
   ...defaultConfig,
-  headers: {
-    get 'x-connect-lib-host'() {
-      return host.origin;
-    },
-  },
+  headers: {},
 };
 
-export default new Request({ http, config });
+http.interceptors.request.use(
+  config => {
+    return {
+      ...config,
+      headers: {
+        ...config.headers,
+        'x-connect-lib-host': host.origin,
+      },
+    };
+  },
+  error => Promise.reject(error),
+);
+
+export default new Request({ http, config: requestConfig });
