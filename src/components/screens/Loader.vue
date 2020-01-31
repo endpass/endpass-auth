8f80095a96d87ddeccccfc8df0112e08eb7c22ce
Alexander Kazimir <alex_kazik@mail.ru>
2020-01-31 14:52:35 +0300

    fix: remove text from global frame loader

--- src/components/screens/Loader.vue
+++ src/components/screens/Loader.vue
@@ -1,26 +1,15 @@
 <template>
-  <v-frame :is-loading="true">
-    <v-description>{{ $t('global.version') }} {{ version }}</v-description>
-  </v-frame>
+  <v-frame :is-loading="true" />
 </template>
 
 <script>
-import pkg from '@/../package.json';
 import VFrame from '@/components/common/VFrame';
-import VDescription from '@/components/common/VDescription';
 
 export default {
   name: 'Loader',
 
-  computed: {
-    version() {
-      return pkg.version;
-    },
-  },
-
   components: {
     VFrame,
-    VDescription,
   },
 };
 </script>
