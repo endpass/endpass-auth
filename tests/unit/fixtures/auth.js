a1e2ef0a74b3f26443e8d8b4bca415be2f650117
Alexander Kazimir <alex_kazik@mail.ru>
2020-01-31 14:56:28 +0300

    test(unit): add Authenticator RegularPasswordCreation module

--- tests/unit/fixtures/auth.js
+++ tests/unit/fixtures/auth.js
@@ -2,4 +2,7 @@ export const code = '111111';
 
 export const email = 'user@email.com';
 
+// ATTENTION: don't forget to update anotherPasswordWithEqualLength
 export const regularPassword = '11111111';
+
+export const anotherPasswordWithEqualLength = '22222222';
