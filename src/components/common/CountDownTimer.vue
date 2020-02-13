5fc90bdb0f78fd912a1c4ee54cceab35416b93a9
Danil Kosterin <raymondcostner@gmail.com>
2020-02-14 02:36:13 +0300

    fix: make CountDownTimer valid

--- src/components/common/CountDownTimer.vue
+++ src/components/common/CountDownTimer.vue
@@ -1,14 +1,83 @@
-import VueTimers from 'vue-timers/mixin'; export default { name:
-'CountDownTimer', props: { timeout: { type: Number, default: 30000, }, isLocked:
-{ type: Boolean, default: false, }, counter: { type: Number, default: 0, }, },
-watch: { isLocked(newValue) { if (!newValue) { if (this.isRequestTimerRunning) {
-this.stopRequestTimer(); } return; } this.startRequestTimer(); }, }, computed: {
-initialCounter() { return this.timeout / 1000; }, isRequestTimerRunning() {
-return this.$timer.request.isRunning; }, }, methods: { startRequestTimer() {
-this.$emit('update:is-locked', true); this.$emit('update:counter',
-this.initialCounter); this.$timer.start('request'); }, stopRequestTimer() {
-this.$emit('update:is-locked', false); this.$timer.stop('request'); },
-onRequestTick() { if (this.counter === 0) { this.stopRequestTimer(); }
-this.$emit('update:counter', this.counter - 1); }, }, mixins: [VueTimers],
-timers: { request: { repeat: true, time: 1000, callback() {
-this.onRequestTick(); }, }, }, };
+<template>
+  <div />
+</template>
+
+<script>
+import VueTimers from 'vue-timers/mixin';
+
+export default {
+  name: 'CountDownTimer',
+
+  props: {
+    timeout: {
+      type: Number,
+      default: 30000,
+    },
+    isLocked: {
+      type: Boolean,
+      default: false,
+    },
+    counter: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  computed: {
+    initialCounter() {
+      return this.timeout / 1000;
+    },
+
+    isRequestTimerRunning() {
+      return this.$timer.request.isRunning;
+    },
+  },
+
+  watch: {
+    isLocked(newValue) {
+      if (!newValue) {
+        if (this.isRequestTimerRunning) {
+          this.stopRequestTimer();
+        }
+
+        return;
+      }
+
+      this.startRequestTimer();
+    },
+  },
+
+  methods: {
+    startRequestTimer() {
+      this.$emit('update:is-locked', true);
+      this.$emit('update:counter', this.initialCounter);
+      this.$timer.start('request');
+    },
+
+    stopRequestTimer() {
+      this.$emit('update:is-locked', false);
+      this.$timer.stop('request');
+    },
+
+    onRequestTick() {
+      if (this.counter === 0) {
+        this.stopRequestTimer();
+      }
+
+      this.$emit('update:counter', this.counter - 1);
+    },
+  },
+
+  mixins: [VueTimers],
+
+  timers: {
+    request: {
+      repeat: true,
+      time: 1000,
+      callback() {
+        this.onRequestTick();
+      },
+    },
+  },
+};
+</script>
