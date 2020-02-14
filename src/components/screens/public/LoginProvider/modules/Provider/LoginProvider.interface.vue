92735d14f5718082bca5110f470127d4071064ae
idudiq <idudiq@gmail.com>
2020-02-14 08:40:50 +0300

    refactor: update structure of login provider

--- src/components/screens/public/LoginProvider/modules/Provider/LoginProvider.interface.vue
+++ src/components/screens/public/LoginProvider/modules/Provider/LoginProvider.interface.vue
@@ -1,5 +1,12 @@
 <template>
-  <login-provider />
+  <login-provider
+    :email="email"
+    :error="error"
+    :is-loading="isLoading"
+    :challenge-type="challengeType"
+    :login-challenge="loginChallenge"
+    @complete="onComplete"
+  />
 </template>
 
 <script>
@@ -7,6 +14,39 @@ import LoginProvider from './LoginProvider.container';
 
 export default {
   name: 'LoginProviderInterface',
+
+  props: {
+    error: {
+      type: String,
+      default: '',
+    },
+
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+
+    loginChallenge: {
+      type: String,
+      default: '',
+    },
+
+    email: {
+      type: String,
+      default: '',
+    },
+
+    challengeType: {
+      type: String,
+      required: true,
+    },
+  },
+
+  methods: {
+    onComplete({ redirect }) {
+      this.$emit('compete', { redirect });
+    },
+  },
 
   components: {
     LoginProvider,
