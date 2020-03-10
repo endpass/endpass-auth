485b299d0f446120788a5d3803497cc3491aaa19
lamartire <lamartire@gmail.com>
2020-03-10 14:55:36 +0300

    feat: add output for new documents scopes

--- src/components/forms/Scopes/scopeTitlesMap.js
+++ src/components/forms/Scopes/scopeTitlesMap.js
@@ -12,4 +12,28 @@ export default {
   'documents:status:read': i18n.t('components.scopes.status'),
   'documents:data:read': i18n.t('components.scopes.content'),
   'documents:image:read': i18n.t('components.scopes.image'),
+  'documents:passport:status:read': i18n.t('components.scopes.passport.status'),
+  'documents:passport:data:read': i18n.t('components.scopes.passport.data'),
+  'documents:passport:image:read': i18n.t('components.scopes.passport.image'),
+  'documents:proof_address:status:read': i18n.t(
+    'components.scopes.proofOfAddress.status',
+  ),
+  'documents:proof_address:data:read': i18n.t(
+    'components.scopes.proofOfAddress.data',
+  ),
+  'documents:proof_address:image:read': i18n.t(
+    'components.scopes.proofOfAddress.image',
+  ),
+  'documents:driver_license:status:read': i18n.t(
+    'components.scopes.driverLicense.status',
+  ),
+  'documents:driver_license:data:read': i18n.t(
+    'components.scopes.driverLicense.data',
+  ),
+  'documents:driver_license:image:read': i18n.t(
+    'components.scopes.driverLicense.image',
+  ),
+  'documents:id_card:status:read': i18n.t('components.scopes.idCard.status'),
+  'documents:id_card:data:read': i18n.t('components.scopes.idCard.data'),
+  'documents:id_card:image:read': i18n.t('components.scopes.idCard.image'),
 };
