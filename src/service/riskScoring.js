055b679ff7088a6f39704111718ebe52cba4e6b9
Danil Kosterin <raymondcostner@gmail.com>
2020-05-27 10:54:24 +0300

    refactor: use promise instead callbacks

--- src/service/riskScoring.js
+++ src/service/riskScoring.js
@@ -3,35 +3,42 @@ import request from '@/class/singleton/request';
 import { METRIC_TYPES } from '@/constants';
 
 /**
+ * @returns {Promise<{{
+ *   [key: string]: string|number,
+ * }}>}
+ */
+const getFingerprintJsonData = () =>
+  new Promise(resolve => {
+    const finger = new Fingerprint({
+      excludeCanvas: true,
+    });
+
+    finger.get((hash, entries) =>
+      resolve(
+        entries.reduce(
+          (accumulator, entry) => ({
+            [entry.key]: entry.value,
+            ...accumulator,
+          }),
+          {},
+        ),
+      ),
+    );
+  });
+
+/**
  * @param {object[]} entries
  * @returns {void}
  */
-const sendFingerprint = () => {
-  const finger = new Fingerprint({
-    excludeCanvas: true,
-  });
+const sendFingerprint = async () => {
+  const json = await getFingerprintJsonData();
 
-  finger.get(async (hash, entries) => {
-    /**
-     * @type {{
-     *   [key: string]: string|number,
-     * }}
-     */
-    const json = entries.reduce(
-      (accumulator, entry) => ({
-        [entry.key]: entry.value,
-        ...accumulator,
-      }),
-      {},
-    );
-
-    await request.post(`${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`, [
-      {
-        payload: btoa(unescape(encodeURIComponent(JSON.stringify(json)))),
-        eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
-      },
-    ]);
-  });
+  await request.post(`${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`, [
+    {
+      payload: btoa(unescape(encodeURIComponent(JSON.stringify(json)))),
+      eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
+    },
+  ]);
 };
 
 export default {
