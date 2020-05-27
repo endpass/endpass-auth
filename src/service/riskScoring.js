c95a932c2f069f47e06d812ed5f1a093234bc355
Danil Kosterin <raymondcostner@gmail.com>
2020-05-27 17:55:10 +0300

    fix: skip permission for user metric request

--- src/service/riskScoring.js
+++ src/service/riskScoring.js
@@ -1,5 +1,5 @@
 import Fingerprint from 'fingerprintjs2';
-import request from '@/class/singleton/request';
+import requestSkipPermission from '@/class/singleton/request/requestSkipPermission';
 import { METRIC_TYPES } from '@/constants';
 
 /**
@@ -33,12 +33,15 @@ const getFingerprintJsonData = () =>
 const sendFingerprint = async () => {
   const json = await getFingerprintJsonData();
 
-  await request.post(`${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`, [
-    {
-      payload: btoa(unescape(encodeURIComponent(JSON.stringify(json)))),
-      eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
-    },
-  ]);
+  await requestSkipPermission.post(
+    `${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`,
+    [
+      {
+        payload: btoa(unescape(encodeURIComponent(JSON.stringify(json)))),
+        eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
+      },
+    ],
+  );
 };
 
 export default {
