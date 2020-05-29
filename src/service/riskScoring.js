47937cd158663d3dff79050ced585500dc5e6c09
Danil Kosterin <raymondcostner@gmail.com>
2020-05-29 23:58:09 +0300

    feat: use utils package for handle metrics

--- src/service/riskScoring.js
+++ src/service/riskScoring.js
@@ -1,70 +1,26 @@
-import Fingerprint from 'fingerprintjs2';
+import getFingerprint from '@endpass/utils/metricsFingerprint';
+import getTimezone from '@endpass/utils/metricsTimezone';
+import encryptMetric from '@endpass/utils/metricsEncrypt';
 import requestSkipPermission from '@/class/singleton/request/requestSkipPermission';
 import { METRIC_TYPES } from '@/constants';
-
-/**
- * @returns {Promise<{{
- *   [key: string]: string|number,
- * }}>}
- */
-const getFingerprintJsonData = () =>
-  new Promise(resolve => {
-    const finger = new Fingerprint({
-      excludeCanvas: true,
-    });
-
-    finger.get((hash, entries) =>
-      resolve(
-        entries.reduce(
-          (accumulator, entry) => ({
-            [entry.key]: entry.value,
-            ...accumulator,
-          }),
-          {},
-        ),
-      ),
-    );
-  });
-
-/**
- * @returns {{
- *   timezone: string,
- *   gmt: number|string,
- * }}
- */
-const getUserTimezone = () => {
-  const dateParts = new Date().toString().split(' ');
-  const timezoneType = dateParts[dateParts.length - 2];
-  const timezoneOffset = dateParts[dateParts.length - 1];
-
-  const timezone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const gmt = `${timezoneType} ${timezoneOffset}`;
-
-  return {
-    timezone,
-    gmt,
-  };
-};
 
 /**
  * @param {object[]} entries
  * @returns {void}
  */
 const sendUserMetrics = async () => {
-  const json = await getFingerprintJsonData();
-  const timezoneData = getUserTimezone();
+  const fingerprint = await getFingerprint();
+  const timezone = getTimezone();
 
-  await requestSkipPermission.post(
+  return requestSkipPermission.post(
     `${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`,
     [
       {
-        payload: btoa(unescape(encodeURIComponent(JSON.stringify(json)))),
+        payload: encryptMetric(fingerprint),
         eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
       },
       {
-        payload: btoa(
-          unescape(encodeURIComponent(JSON.stringify(timezoneData))),
-        ),
+        payload: encryptMetric(timezone),
         eventType: METRIC_TYPES.USER_BROWSER_TIMEZONE,
       },
     ],
