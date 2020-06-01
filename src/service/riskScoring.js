743c498ef3fb4f0d21aacee3bfad21847635a55f
Danil Kosterin <raymondcostner@gmail.com>
2020-06-01 18:32:17 +0300

    feat: add ip metric, mock fix

--- src/service/riskScoring.js
+++ src/service/riskScoring.js
@@ -1,29 +1,48 @@
-import getFingerprint from '@endpass/utils/metricsFingerprint';
-import getTimezone from '@endpass/utils/metricsTimezone';
-import encryptMetric from '@endpass/utils/metricsEncrypt';
+import {
+  getIP,
+  getFingerprint,
+  getTimezone,
+  encryptMetric,
+} from '@endpass/utils/metrics';
 import requestSkipPermission from '@/class/singleton/request/requestSkipPermission';
 import { METRIC_TYPES } from '@/constants';
 
 /**
- * @param {object[]} entries
- * @returns {void}
+ * @returns {Promise<void>}
  */
 const sendUserMetrics = async () => {
-  const fingerprint = await getFingerprint();
-  const timezone = getTimezone();
+  const metrics = [];
+
+  try {
+    const ip = await getIP();
+
+    metrics.push({
+      eventType: METRIC_TYPES.USER_IP_WEBRTC,
+      payload: encryptMetric(ip),
+    });
+  } catch (err) {}
+
+  try {
+    const fingerprint = await getFingerprint();
+
+    metrics.push({
+      eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
+      payload: encryptMetric(fingerprint),
+    });
+  } catch (err) {}
+
+  try {
+    const timezone = getTimezone();
+
+    metrics.push({
+      eventType: METRIC_TYPES.USER_BROWSER_TIMEZONE,
+      payload: encryptMetric(timezone),
+    });
+  } catch (err) {}
 
   return requestSkipPermission.post(
     `${ENV.VUE_APP_IDENTITY_API_URL}/user/metric`,
-    [
-      {
-        payload: encryptMetric(fingerprint),
-        eventType: METRIC_TYPES.USER_BROWSER_FINGERPRINT,
-      },
-      {
-        payload: encryptMetric(timezone),
-        eventType: METRIC_TYPES.USER_BROWSER_TIMEZONE,
-      },
-    ],
+    metrics,
   );
 };
 
--- tests/unit/mocks/service/riskScoring.mock.js
+++ tests/unit/mocks/service/riskScoring.mock.js
@@ -2,7 +2,7 @@ import riskScoringService from '@/service/riskScoring';
 
 jest.mock('@/service/riskScoring', () => {
   return {
-    sendFingerprint: jest.fn(),
+    sendUserMetrics: jest.fn(),
   };
 });
 
