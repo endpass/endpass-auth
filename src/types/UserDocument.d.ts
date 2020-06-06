feefe178ad48e14db231277ce5c625d45f34ffa5
dudiq <idudiq@gmail.com>
2020-06-06 06:03:14 +0300

    fix: declrate UserDocument type

--- src/types/UserDocument.d.ts
+++ src/types/UserDocument.d.ts
@@ -1,36 +1,27 @@
-// import { DOC_STATUSES } from '@/constants';
+import { DOC_STATUSES } from '@/constants';
 
-// hacky solution for correct working of declaring types,
-// import { DOC_STATUSES } from '@/constants'; does not working here
-type DOC_STATUSES = {
-  DRAFT: 'Draft';
-  RECOGNITION: 'Recognition';
-  PENDING_REVIEW: 'PendingReview';
-  NOT_READABLE: 'NotReadable';
-  NOT_VERIFIED: 'NotVerified';
-  VERIFIED: 'Verified';
-};
+type DOC_STATUSES_ENUM = typeof DOC_STATUSES[keyof typeof DOC_STATUSES];
 
-// type DOC_STATUSES_ENUM = typeof DOC_STATUSES[keyof typeof DOC_STATUSES];
+declare global {
+  type UserDocument = {
+    id: string;
+    createdAt: number;
+    status: DOC_STATUSES_ENUM;
+    documentType: string;
+    areaType: string;
+    description: string;
+    firstName: string;
+    lastName: string;
+    number: number;
+    dateOfBirth: number;
+    dateOfIssue: number;
+    dateOfExpiry: number;
+    issuingCountry: string;
+    issuingAuthority: string;
+    issuingPlace: string;
+    address: string;
+    imgPath: string;
+    rejectReason?: string;
+  };
 
-type UserDocument = {
-  id: string;
-  createdAt: number;
-  // status: DOC_STATUSES_ENUM;
-  status: DOC_STATUSES[keyof DOC_STATUSES];
-  documentType: string;
-  areaType: string;
-  description: string;
-  firstName: string;
-  lastName: string;
-  number: number;
-  dateOfBirth: number;
-  dateOfIssue: number;
-  dateOfExpiry: number;
-  issuingCountry: string;
-  issuingAuthority: string;
-  issuingPlace: string;
-  address: string;
-  imgPath: string;
-  rejectReason?: string;
-};
+}
